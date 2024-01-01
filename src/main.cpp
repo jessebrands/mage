e0009642e9e1e55ac386e34c888f4040a9695c79
Jesse Gerard Brands <jessebrands@icloud.com>
2024-01-01 22:36:57 +0100

    more like vulkan't

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,12 @@
 #include <windows.h>
 
 #include <mage/platform/win32/win32_window.hpp>
-#include <mage/renderer/vulkan/vk_instance.hpp>
-
-#include <mage/renderer/render_system.hpp>
-
-#include "mage/platform/win32/vk_win32_surface.hpp"
-#include "mage/renderer/vulkan/vk_device.hpp"
 
 int APIENTRY WinMain([[maybe_unused]] HINSTANCE hinstance,
                      [[maybe_unused]] HINSTANCE prev_instance,
                      [[maybe_unused]] PSTR cmd_line,
                      [[maybe_unused]] int show_cmd) {
     mage::win32_window window(hinstance, show_cmd);
-    mage::vk_instance instance;
-    mage::vk_surface surface(instance, window, hinstance);
-    const auto devices = instance.enumerate_physical_devices();
-    const auto selected_device = devices[0];
-    const auto queue_indices = selected_device.get_queue_families(surface);
-    mage::vk_device device(selected_device, queue_indices);
 
     MSG msg;
     BOOL ret;
